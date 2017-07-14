995418aaa4cb3070347d2d735aa26ef3afb9a5f7
ShinyVerse <playeronereadystart@gmail.com>
2017-07-14 17:50:34 +0100

    Makes use of getDay() method. Returns a quote for today depending on day of the week. Once button has been clicked.

---
+++ MadeUpChallenges/0 JS MADEUP CHALLENGES/ArrayListResultDependentOnCurrentDay/behaviour.js
@@ -0,0 +1,31 @@
+var myAdviceList = ["Marry someone with a different favourite cereal to you so they won't eat yours",
+                    "Never shout at someone unless something's on fire",
+                    "You're not weird, you're limited edition",
+                    "Your pants never get too tight if you don't wear any",
+                    "Know your worth...then add tax",
+                    "If you have to hang up on someone do it in the middle of one of your own sentences.",
+                    "Whatever you do, always give 100%. Unless you are donating blood."
+                  ];
+
+var paragraph = document.getElementById('adviceHere');
+var adviceButton = document.getElementById('AdviceClicker')
+
+
+function getToday() {
+  var today = new Date();
+  var dayNum = today.getDay();
+  console.log(dayNum);
+  return dayNum;
+}
+
+adviceButton.onclick = function(){
+  paragraph.textContent = myAdviceList[getToday()];
+
+  
+}
+
+
+// function randomize() {
+//   var randomNum = Math.floor( Math.random() * myAdviceList.length);
+//   return randomNum;
+// }
