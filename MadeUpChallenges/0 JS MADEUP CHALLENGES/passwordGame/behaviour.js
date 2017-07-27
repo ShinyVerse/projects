c8b79052d725bcae4b267927f55bf5e9db1ca4c9
ShinyVerse <playeronereadystart@gmail.com>
2017-07-27 17:00:49 +0100

    Needs some work on button styles, but otherwise functions as it should (...for now.)

--- MadeUpChallenges/0 JS MADEUP CHALLENGES/passwordGame/behaviour.js
+++ MadeUpChallenges/0 JS MADEUP CHALLENGES/passwordGame/behaviour.js
@@ -9,6 +9,8 @@ var userFirstLetter = document.getElementById('firstLetter');
 var userSecondLetter = document.getElementById('secondLetter');
 var userThirdLetter = document.getElementById('thirdLetter');
 
+var answerInfo = document.getElementById('answerInfo');
+
 var ans1 = "A";
 var ans2 = "R";
 var ans3 = "T";
@@ -22,7 +24,37 @@ returnButton.onclick = function() {
 }
 
 codeGuessButton.onclick = function() {
-  console.log(userFirstLetter.value);
-  console.log(userSecondLetter.value);
-  console.log(userThirdLetter.value);
-}
+  var correctAns = 0;
+  var incorrect = 0;
+    if (userFirstLetter.value.toUpperCase() === ans1){
+      correctAns++;
+    } else {
+      incorrect++;
+    }
+    if (userSecondLetter.value.toUpperCase() === ans2){
+      correctAns++;
+    } else {
+      incorrect++;
+    }
+    if (userThirdLetter.value.toUpperCase() === ans3){
+      correctAns++;
+    } else {
+      incorrect++;
+    }
+    if (correctAns === 3) {
+      answerInfo.textContent = "You cracked the code!";
+      var myDiv = document.getElementById('guessContainer');
+      myDiv.innerHTML = myDiv.innerHTML + "<button type='button' id='finalButton'>Take me to my Prize!</button><br>";
+      var finalButton = document.getElementById('finalButton');
+      finalButton.onclick = function(){
+        location.href = "secret.html";
+      }
+    } else {
+      answerInfo.textContent = "Access Denied. You got " + incorrect + " incorrect. Please try again!";
+      correctAns = 0;
+      incorrect = 0;
+      userFirstLetter.value = "";
+      userSecondLetter.value  = "";
+      userThirdLetter.value  = "";
+    }
+  }
