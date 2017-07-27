88e5efb0c4bac4b53815a00230000d44e9bec4b3
ShinyVerse <playeronereadystart@gmail.com>
2017-07-27 13:20:35 +0100

    More added to the guessBox div and some JS to allow for box to be hidden, allows 3 guess of 3 letters, has a return button to clear the guessBox from view.

--- MadeUpChallenges/0 JS MADEUP CHALLENGES/passwordGame/behaviour.js
+++ MadeUpChallenges/0 JS MADEUP CHALLENGES/passwordGame/behaviour.js
@@ -1,6 +1,28 @@
 var passwordButton = document.getElementById('passwordButton');
 var passwordDivBox = document.getElementById('emptyOne');
 
+var codeGuessButton = document.getElementById('codeGuessButton');
+
+var returnButton = document.getElementById('returnButton')
+
+var userFirstLetter = document.getElementById('firstLetter');
+var userSecondLetter = document.getElementById('secondLetter');
+var userThirdLetter = document.getElementById('thirdLetter');
+
+var ans1 = "A";
+var ans2 = "R";
+var ans3 = "T";
+
 passwordButton.onclick = function() {
   passwordDivBox.setAttribute('id', 'guessBox');
+}
+
+returnButton.onclick = function() {
+  passwordDivBox.setAttribute('id', 'emptyOne')
+}
+
+codeGuessButton.onclick = function() {
+  console.log(userFirstLetter.value);
+  console.log(userSecondLetter.value);
+  console.log(userThirdLetter.value);
 }
