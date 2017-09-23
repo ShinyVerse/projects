e2a5604799ee6d3fa36c13996c9ab79f2ee2aac6
ShinyVerse <playeronereadystart@gmail.com>
2017-09-23 17:56:45 +0100

    Clean up

--- MadeUpChallenges/0 JS MADEUP CHALLENGES/inputNextDoorJQUERY/myScript.js
+++
@@ -1,8 +0,0 @@
-var array = [ 'beans', 'salmon', 'eggs'];
-
-$(function() {
-  $('#generateButton').on('click', function() {
-    var what = $(this).closest().attr['name="firstTrait"'];
-    alert(what);
-  })
-});
---
+++ teamTreeHouse/video03/js/myjs.js
@@ -0,0 +1,9 @@
+$(".animsition").animsition({
+  inClass: 'flip-in-x',
+  outClass: 'flip-out-y',
+  linkElement: 'header a',
+  inDuration: 1000,
+  outDuration: 500
+});
+
+$('.header').sticky();
