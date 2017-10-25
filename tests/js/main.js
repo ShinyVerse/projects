01160940ed987d446d5e3a28a0f3e3feec21f2f8
Laura <playeronereadystart@gmail.com>
2017-10-25 21:39:58 +0100

    span fixed. No globals!

---
+++ tests/js/main.js
@@ -0,0 +1,25 @@
+var id = '#8155a8df-dac3-4089-91c7-e31190628280';
+
+// Add the main div id
+$('.outerContainer').append($("<div id ='" + id +  "' class='listingBox'></div>"));
+
+// Add feature div
+
+$('#' + id).append($("<div class='feature'></div>")
+
+// var $listingBox = ;
+//
+// $listingBox.append($("<div class='feature'></div>").append("<img src= '" + image + "'/>" ));
+//
+// var myDivId = $('#' + id);
+// myDivId.find().closest('.feature').append($spanP);
+// console.log(myDivId);
+//
+// // var myfakeId = $('#myFakeid');
+// // console.log(myfakeId);
+//
+// var myFeature = myId.find('.feature');
+// console.log(myFeature);
+//
+// var x = myFeature.append('<p>my P!</p>');
+// console.log(x);
--- yourCanteen/playAbout/js/main.js
+++ yourCanteen/playAbout/js/main.js
@@ -1,61 +1,59 @@
+function createPageData(data) {
+  $.each(data.dishList, function(key, value) {
 
-var $feature = $("<div class='feature'></div>");
-var $spanP = $("<span><p></p></span>");
-
-$.getJSON('https://api-staging.yourcanteen.com/dishes?latitude=51.5074&longitude=-0.127758&radius=10000&date='+ showCurrentDate() +'', function(data){
-
-  $.each(data.dishList, function(key, value){
-
-        console.log(value.images[0]);
-        var image = value.images[0];
-        var id  = value.id;
-        var $listingBox = $("<div id ='" + id +  "' class='listingBox'></div>");
-
-        $listingBox.append($("<div class='feature'></div>").append("<img src= '" + image + "'/>" ));
-        $spanP.insertAfter($(".feature img"));
-        $(".outerContainer").append($listingBox);
+    console.log(value.images[0]);
+    var image = value.images[0];
+    var id = value.id;
+    var $listingBox = $("<div id ='" + id + "' class='listingBox'></div>");
+    var $feature = $("<div class='feature'></div>");
+    var $spanP = $("<span><p></p></span>");
+    var $image = $("<img src= '" + image + "'/>");
+    $feature.append($image);
+    $feature.append($spanP);
+    $listingBox.append($feature);
+    $(".outerContainer").append($listingBox);
   });
+}
+
+$.getJSON('https://api-staging.yourcanteen.com/dishes?latitude=51.5074&longitude=-0.127758&radius=10000&date=' + showCurrentDate() + '', function(data) {
+
+  createPageData(data);
+
 });
 
-function callDaysData(){
-  if ($("#today").hasClass("selected")){
-    $.getJSON('https://api-staging.yourcanteen.com/dishes?latitude=51.5074&longitude=-0.127758&radius=10000&date='+ showCurrentDate() +'', function(data){
-      $.each(data.dishList, function(i, value){
-          var image = value.images[0];
-            $(".outerContainer").append("<img src= '" + image + "'/>");
-      });
-      });
+function callDaysData() {
+  if ($("#today").hasClass("selected")) {
+    $.getJSON('https://api-staging.yourcanteen.com/dishes?latitude=51.5074&longitude=-0.127758&radius=10000&date=' + showCurrentDate() + '', function(data) {
+    createPageData(data);
+    });
 
-  } else if ($("#tomorrow").hasClass("selected")){
-    $.getJSON('https://api-staging.yourcanteen.com/dishes?latitude=51.5074&longitude=-0.127758&radius=10000&date='+ showTomorrowDate() +'', function(data){
-      $.each(data.dishList, function(i, value){
-          var image = value.images[0];
-            $(".outerContainer").append("<img src= '" + image + "'/>");
-      });
-      });
+  } else if ($("#tomorrow").hasClass("selected")) {
+    $.getJSON('https://api-staging.yourcanteen.com/dishes?latitude=51.5074&longitude=-0.127758&radius=10000&date=' + showTomorrowDate() + '', function(data) {
+    createPageData(data);
+    });
   }
 }
 
 
-  function showCurrentDate(){
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
+function showCurrentDate() {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
 
-     return newdate = year + "-" + month + "-" + day;
-  }
+  return newdate = year + "-" + month + "-" + day;
+}
 
-  function showTomorrowDate(){
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate()+1;
-    var year = dateObj.getUTCFullYear();
+function showTomorrowDate() {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate() + 1;
+  var year = dateObj.getUTCFullYear();
 
-     return newdate = year + "-" + month + "-" + day;
-  }
+  return newdate = year + "-" + month + "-" + day;
+}
 
-$("#tomorrow").on("click", function(){
+$("#tomorrow").on("click", function() {
   $("#today").removeClass("selected")
   $(this).addClass("selected");
   $(".sampleBox").empty();
@@ -63,7 +61,7 @@ $("#tomorrow").on("click", function(){
 });
 
 
-$("#today").on("click", function(){
+$("#today").on("click", function() {
   $("#tomorrow").removeClass("selected")
   $(this).addClass("selected");
   $(".sampleBox").empty();
