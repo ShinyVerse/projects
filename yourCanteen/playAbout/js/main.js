f28ca1a6988758fea498a1cb3ee800fe3b291525
Laura <playeronereadystart@gmail.com>
2017-11-01 15:07:29 +0000

    Half way to sorting out countdown timer

--- yourCanteen/playAbout/js/main.js
+++ yourCanteen/playAbout/js/main.js
@@ -1,6 +1,45 @@
+var userLocation = {
+  "latitude": 51.535256,
+  "longitude": -0.089363
+};
+
+restaurantLoc = {
+                 "latitude":51.53169,
+                 "longitude":-0.09449
+              };
+
+var distance = distance(userLocation.latitude, userLocation.longitude, restaurantLoc.latitude, restaurantLoc.longitude);
+var shownDistance = (Math.round(distance*10)/10);  //displays 343.548
+
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	if (unit=="K") { dist = dist * 1.609344 }
+	if (unit=="N") { dist = dist * 0.8684 }
+	return dist
+}
+
 function changeToPrice(price){
   return price.toFixed(2);
 }
+
+
+  var d = new Date;
+  d.getHours(); // => 9
+  d.getMinutes(); // =>  30
+  d.getSeconds(); // => 51
+
+  console.log(d.getHours() );
+  console.log(d.getMinutes())
+
+
 
 function createPageData(data) {
 
@@ -12,8 +51,11 @@ function createPageData(data) {
     var nowPrice = value.price.value /100;
     var nameOfDish = value.name;
     var chefId = value.chefId;
-
+    var chefProfileImage = data.chefIdToChefInfo["e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62"].profileImage;
     var nameOfRestaurant = data.chefIdToChefInfo["e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62"].kitchenName;
+    var timeSlot = data.chefIdToChefInfo["e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62"].deliveryTimes[0].slots[1];
+    var countdown = timeSlot.slice(-5);
+
 
     var starRatingID = 0;
     var image = value.images[0];
@@ -21,15 +63,16 @@ function createPageData(data) {
     var $sampleImage = $('<img src="img/chef1.jpg" alt="Chef profile picture" class="chefPic"/>')
     var $listingBox = $("<div id ='" + id + "' class='listingBox'></div>");
     var $feature = $("<div class='feature'></div>");
-    var $spanP = $("<span><p>Deliv info</p></span>");
+    var $spanP = $("<span><p>" + countdown +"</p></span>");
     var $image = $("<img src= '" + image + "'/>");
 
     var $infoListing = $("<div class='info-listing'></div>");
-    var $chefImage = $("<img src='img/chef1.jpg' alt='Chef profile picture' class='chefPic'/>");
+    var $chefImage = $("<img src='"+ chefProfileImage +"' alt='Chef profile picture' class='chefPic'/>");
+    console.log(chefProfileImage);
     var $restaurantName =   $('<p class="restaurantName">'+ nameOfRestaurant +'</p>');
     var $starRate = $('<p id="distance'+ starRatingID +'">&#x2605; &#x2605; &#x2605; &#x2605; &#x2606;</p>');
     var $distanceSlash = $('<p class="distanceSlash">/</p>');
-    var $actualDistance = $('<p id="actualDistance'+ starRatingID + '">&#x2662; 1m</p></br>');
+    var $actualDistance = $('<p id="actualDistance'+ starRatingID + '">&#x2662;'+ shownDistance +'</p></br>');
     var $divPrice = $('<div class="pricing"></div>');
     //append to divPrice
     var $origAndCurrentPrice = $('<p class="origPrice">£'+ changeToPrice(oldPrice) +'</p><p class="currentPrice">£'+ changeToPrice(nowPrice) +'</p>');
@@ -39,7 +82,7 @@ function createPageData(data) {
 
     $feature.append($image);
     $feature.append($spanP);
-    $infoListing.append($sampleImage);
+    $infoListing.append($chefImage);
     $infoListing.append($restaurantName);
     $infoListing.append($starRate);
     $infoListing.append($distanceSlash);
