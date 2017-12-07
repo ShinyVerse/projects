c8c74159997f6325789775c7ac8e3027612f1fef
Laura <playeronereadystart@gmail.com>
2017-12-07 16:44:09 +0000

    +/- added. Next: displaying the sum.

--- reactPractise/timestable/src/TimesTables.js
+++ reactPractise/timestable/src/TimesTables.js
@@ -16,15 +16,29 @@ class TimesTables extends Component {
   updateMinus(){
     this.setState({val: this.state.val - 9})
   }
+
+  componentWillMount(){
+    console.log('componentWillMount');
+  }
+
   render(){
     console.log('render');
     return <div>
             <h1>The 9 times table is the best, so we'll start with that:</h1>
             <button onClick={this.update}>+</button>
             <h1>{this.state.val}</h1>
+            {/* <h2> is {this.state.val} x 9</h2> */}
             <button onClick={this.updateMinus}>-</button>
           </div>
   }
+  componentDidMount(){
+    console.log('componentDidMount');
+    // this.inc = setInterval(this.update, 1000)
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+  }
+
 }
 
 export default TimesTables;
