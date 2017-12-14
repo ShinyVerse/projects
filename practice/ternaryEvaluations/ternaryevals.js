3771bcb0592f9e626f7c043a722cc22c9cc3755d
Laura <playeronereadystart@gmail.com>
2017-12-14 13:06:01 +0000

    ternary evaluations: multiple

---
+++ practice/ternaryEvaluations/ternaryevals.js
@@ -0,0 +1,6 @@
+var firstCheckPassed = true,
+    secondCheckPassed = false,
+    access = firstCheckPassed ? 'Access denied' : secondCheckPassed ? 'Access granted': 'Access denied';
+
+
+console.log(access)
