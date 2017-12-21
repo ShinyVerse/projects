e3c7e151457f497a21272c37c9f014491cb39364
Laura <playeronereadystart@gmail.com>
2017-12-21 13:04:25 +0000

    props > state working fine.

--- yourCanteen/newcanteen/src/App.js
+++ yourCanteen/newcanteen/src/App.js
@@ -1,53 +1,67 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Display from './Display';
 import DayChoice from './DayChoice';
 
 import './App.css';
 
-
-const PRODUCTS = {"dishList":[
+const PRODUCTS = {
+  "dishList": [
     {
-       "id":"6d272180-fac9-4dfe-a918-f94ae6beb2a6",
-       "description":"Slow stewed chicken in coconut water, with fresh water chestnuts, coconut chunks, goji berry and Chinese date\nDelivered hot but can be consumed cold as well, a chicken dip is included for extra flavour to the chicken\n\nabsolutely no MSG\ndelivered in 375ml sealed container",
-       "chefId":"59fc635b-9567-4bc0-916f-0bea6f234ff6",
-       "dishType":"LUNCH",
-       "name":"海南椰子鸡汤 Hainan Chicken Soup in Coconut water",
-       "cuisine":"Chinese",
-       "countOfReviews":0,
-       "price":{
-          "currencyCode":"GBP",
-          "value":650
-       },
-       "images":[
-          "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/dish/6d272180-fac9-4dfe-a918-f94ae6beb2a6/1.jpeg"
-       ],
-       "availability":{
-          "2017-12-18":{
-             "availableDateTime":"2017-12-18T23:59:18",
-             "orderUntilDateTime":"2017-12-18T23:59:18",
-             "totalCapacity":20,
-             "countOfRecentOrder":20
-          },
-          "2017-12-17":{
-             "availableDateTime":"2017-12-17T23:59:18",
-             "orderUntilDateTime":"2017-12-17T22:17:18",
-             "totalCapacity":10,
-             "countOfRecentOrder":9
-          }
-       }
+      "id": "6d272180-fac9-4dfe-a918-f94ae6beb2a6",
+      "description": "Slow stewed chicken in coconut water, with fresh water chestnuts, coconut chunks, goji berry and Chinese date\nDelivered hot but can be consumed cold as well, a chicken dip is included for extra flavour to the chicken\n\nabsolutely no MSG\ndelivered in 375ml sealed container",
+      "chefId": "59fc635b-9567-4bc0-916f-0bea6f234ff6",
+      "dishType": "LUNCH",
+      "name": "海南椰子鸡汤 Hainan Chicken Soup in Coconut water",
+      "cuisine": "Chinese",
+      "countOfReviews": 0,
+      "price": {
+        "currencyCode": "GBP",
+        "value": 650
+      },
+      "images": ["http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/dish/6d272180-fac9-4dfe-a918-f94ae6beb2a6/1.jpeg"],
+      "availability": {
+        "2017-12-18": {
+          "availableDateTime": "2017-12-18T23:59:18",
+          "orderUntilDateTime": "2017-12-18T23:59:18",
+          "totalCapacity": 20,
+          "countOfRecentOrder": 20
+        },
+        "2017-12-17": {
+          "availableDateTime": "2017-12-17T23:59:18",
+          "orderUntilDateTime": "2017-12-17T22:17:18",
+          "totalCapacity": 10,
+          "countOfRecentOrder": 9
+        }
+      }
     }
- ]
+  ]
 }
 
-
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      today: true
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (!e.target.className) {
+      this.setState(prevState => ({
+        today: !prevState.today
+      }));
+    } else {
+      e.preventDefault();
+      console.log(e.target.className);
+    }
+  }
   render() {
     return (
       <div className="App">
-        <DayChoice />
-        <Display products={PRODUCTS}  />
+        <DayChoice handleClick={this.handleClick} today={this.state.today}/>
+        <Display products={PRODUCTS}/>
       </div>
     );
   }
--- yourCanteen/newcanteen/src/DayChoice.js
+++ yourCanteen/newcanteen/src/DayChoice.js
@@ -3,38 +3,20 @@ import React, {Component} from 'react';
 import './DayChoice.css';
 
 class DayChoice extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      today: true
-    };
 
-    this.handleClick = this.handleClick.bind(this);
-}
-
-handleClick(e) {
-  if (!e.target.className){
-
-  this.setState(prevState => ({
-    today: !prevState.today}));
-} else {
-  e.preventDefault();
-console.log(e.target.className);
-}
-}
 
 render() {
-  const today = this.state.today;
+  const today = this.props.today;
 
   let link = null;
   let link2 = null
   if (today) {
-      link = <a className="selected" onClick={this.handleClick}>Today</a>
-      link2 = <a className="" onClick={this.handleClick}>Tomorrow</a>
+      link = <a className="selected" onClick={this.props.handleClick}>Today</a>
+      link2 = <a className="" onClick={this.props.handleClick}>Tomorrow</a>
 
   } else {
-      link = <a className="" onClick={this.handleClick}>Today</a>
-      link2 = <a className="selected" onClick={this.handleClick}>Tomorrow</a>
+      link = <a className="" onClick={this.props.handleClick}>Today</a>
+      link2 = <a className="selected" onClick={this.props.handleClick}>Tomorrow</a>
   }
 
   return (
