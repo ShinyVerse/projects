8ffa6a792c0b5f92fe41b0cf3ded2406339e12ec
Laura <playeronereadystart@gmail.com>
2018-01-04 11:27:18 +0000

    Added new mock Json

--- yourCanteen/newcanteen/src/App.js
+++ yourCanteen/newcanteen/src/App.js
@@ -1,25 +1,168 @@
 import React, {Component} from 'react';
 import Display from './Display';
-import DisplayMenu from './DisplayMenu';
+
 import DayChoice from './DayChoice';
 
 import './App.css';
 
 const PRODUCTS = {
-  "dishList": [
-  {
+  "dishList":[
+     {
+        "id":"8155a8df-dac3-4089-91c7-e31190628280",
+        "description":"香菇小白菜烧豆腐",
+        "chefId":"59fc635b-9567-4bc0-916f-0bea6f234ff6",
+        "dishType":"LUNCH",
+        "name":"Chinese Mushroom with Pak Choi and Tofu",
+        "cuisine":"Chinese",
+        "countOfReviews":0,
+        "originalPrice":{
+           "currencyCode":"GBP",
+           "value":780
+        },
+        "price":{
+           "currencyCode":"GBP",
+           "value":700
+        },
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/dish/10.jpg"
+        ]
+     },
+     {
+        "id":"8155a8df-dac3-4089-91c7-e31190628281",
+        "description":"Tasty for you",
+        "chefId":"59fc635b-9567-4bc0-916f-0bea6f234ff7",
+        "dishType":"LUNCH",
+        "name":"Thai Tofu",
+        "cuisine":"Thai",
+        "countOfReviews":0,
+        "originalPrice":{
+           "currencyCode":"GBP",
+           "value":800
+        },
+        "price":{
+           "currencyCode":"GBP",
+           "value":750
+        },
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/dish/10.jpg"
+        ]
+     },
+     {
+        "id":"8155a8df-dac3-4089-91c7-e31190628282",
+        "description":"Pasta, done like mama used to done.",
+        "chefId":"59fc635b-9567-4bc0-916f-0bea6f234ff8",
+        "dishType":"DINNER",
+        "name":"Pasta",
+        "cuisine":"Italian",
+        "countOfReviews":0,
+        "originalPrice":{
+           "currencyCode":"GBP",
+           "value":600
+        },
+        "price":{
+           "currencyCode":"GBP",
+           "value":500
+        },
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/dish/10.jpg"
+        ]
+     },
+     {
+        "id":"8155a8df-dac3-4089-91c7-e31190628283",
+        "description":"Start your day with spicy.",
+        "chefId":"59fc635b-9567-4bc0-916f-0bea6f234ff9",
+        "dishType":"BREAKFAST",
+        "name":"Spicy Tandoori Vegetables With Rice",
+        "cuisine":"Indian",
+        "countOfReviews":0,
+        "originalPrice":{
+           "currencyCode":"GBP",
+           "value":400
+        },
+        "price":{
+           "currencyCode":"GBP",
+           "value":350
+        },
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/dish/10.jpg"
+        ]
+     }
+  ],
+  "chefIdToChefInfo":{
+     "59fc635b-9567-4bc0-916f-0bea6f234ff6":{
+        "id":"59fc635b-9567-4bc0-916f-0bea6f234ff6",
+        "profileImage":"http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/logo.jpg",
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/5.jpeg"
+        ],
+        "name":"Eat Fan",
+        "phoneNumber":"+447887525658",
+        "synopsis":"We are an independent lunch provider serving workers at Canary Wharf area\nWe own a 5-star food hygiene rated kitchen nearby\nWe only cook food that we love\nWe guarantee quality, tasty and healthy in every box",
+        "kitchenName":"Eat Fan",
+        "deliveryTypes":[
+           "COLLECT"
+        ],
+        "rating":5.0,
+        "countOfReviews":1
+     },
+     "59fc635b-9567-4bc0-916f-0bea6f234ff7":{
+        "id":"59fc635b-9567-4bc0-916f-0bea6f234ff7",
+        "profileImage":"http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/logo.jpg",
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/5.jpeg"
+        ],
+        "name":"Gorm Nosh",
+        "phoneNumber":"+447887525658",
+        "synopsis":"We are an independent lunch provider",
+        "kitchenName":"Gorm Nosh",
+        "deliveryTypes":[
+           "COLLECT"
+        ],
+        "rating":4.5,
+        "countOfReviews":1
+     },
+     "59fc635b-9567-4bc0-916f-0bea6f234ff9":{
+        "id":"59fc635b-9567-4bc0-916f-0bea6f234ff9",
+        "profileImage":"http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/logo.jpg",
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/5.jpeg"
+        ],
+        "name":"IndianCow",
+        "phoneNumber":"+447887525658",
+        "synopsis":"We make good spice, no bad time toilet after you eat.",
+        "kitchenName":"IndianCow",
+        "deliveryTypes":[
+           "COLLECT"
+        ],
+        "rating":4.9,
+        "countOfReviews":1
+     },
+     "59fc635b-9567-4bc0-916f-0bea6f234ff8":{
+        "id":"59fc635b-9567-4bc0-916f-0bea6f234ff8",
+        "profileImage":"http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/logo.jpg",
+        "images":[
+           "http://assets-staging.yourcanteen.com/chef/e5eb1ccf-bd45-4d01-8e2a-0d10cc275f62/profile/5.jpeg"
+        ],
+        "name":"ItaliLion",
+        "phoneNumber":"+447887525658",
+        "synopsis":"Pasta, mmm, delicious pasta.",
+        "kitchenName":"ItaliLion",
+        "deliveryTypes":[
+           "COLLECT"
+        ],
+        "rating":3.2,
+        "countOfReviews":1
+     }
 
-      "dishType": "LUNCH"
+  },
 
-    }, {
-      "dishType": "DINNER"
-    }, {
-      "dishType": "BREAKFAST"
-    }
-
-
-]
+  "pageInfo":{
+     "countOfResults":1,
+     "limit":10,
+     "offset":0
+  }
 }
+
 
 class App extends Component {
 constructor(props) {
--- yourCanteen/newcanteen/src/YourCanteen.js
+++ yourCanteen/newcanteen/src/YourCanteen.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Anything extends Component {
   render() {
 
-    const idOf = this.props.products.dishList[0].dishType;
+    const idOf0 = this.props.products.dishList[0].images;
+    const idOf1 = this.props.products.dishList[1].dishType;
+    const idOf2 = this.props.products.dishList[2].dishType;
+    const idOf3 = this.props.products.dishList[3].dishType;
     return (
-    <h1>{idOf}</h1>
+      <div>
+          <img src={idOf0} alt={this.props.products.dishList[0].cuisine}></img>
+          <h1>{idOf1}</h1>
+          <h1>{idOf2}</h1>
+          <h1>{idOf3}</h1>
+      </div>
+
     );
   }
 }
