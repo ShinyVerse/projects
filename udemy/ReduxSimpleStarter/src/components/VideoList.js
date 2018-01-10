1817be70fd6157949acc555cf73f1259f995637b
Laura <playeronereadystart@gmail.com>
2018-01-10 20:08:24 +0000

    State/props covered

---
+++ udemy/ReduxSimpleStarter/src/components/VideoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import VideoListItem from './VideoListItem.js'
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+  console.log(videoItems);
+  //imagine from props we get:
+  //const videos = props.videos; << array of videos
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+      {/*  {props.videos.length} //If changed from a function based component to a class based
+      //we would need to change props.videos.length to this.props.videos.length */}
+    </ul>
+  );
+}
+
+export default VideoList;
--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar.js'
+import YTSearch from 'youtube-api-search';
 
-const apiKey = 'AIzaSyCHhiRTdRhMxZ4t_1RXis82RPupe-cDkg0';
+import SearchBar from './components/SearchBar.js'
+import VideoList from './components/VideoList.js'
+
+
+const API_KEY = 'AIzaSyCHhiRTdRhMxZ4t_1RXis82RPupe-cDkg0';
+
+
 //create a new component. This component should produce some html.
 //some html
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+       this.setState({ videos });
+
+    });
+
+  }
+  render(){
   return (
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>
   );
 }
-//Take this component's generated HTML and put it
+}
+//Take this component's generated  HTML and put it
 //on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
