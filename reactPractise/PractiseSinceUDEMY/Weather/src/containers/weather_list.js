9885e755a20793e8991829b23d8eb89520cceb64
Laura <playeronereadystart@gmail.com>
2018-01-25 11:26:28 +0000

    Charts up and running. Re-usable component - chart. Using Sparklines

--- reactPractise/PractiseSinceUDEMY/Weather/src/containers/weather_list.js
+++ reactPractise/PractiseSinceUDEMY/Weather/src/containers/weather_list.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import  Chart from '../components/chart';
+
 
 class WeatherList extends Component {
 renderWeather(cityData){
   const name = cityData.city.name;
   const temps = cityData.list.map(weather => weather.main.temp);
+  // const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (temp - 273));
   console.log(temps);
+  const pressures = cityData.list.map(weather => weather.main.pressure);
+  const humidities = cityData.list.map(weather => weather.main.humidity);
+
 
 
 return (
@@ -15,9 +20,13 @@ return (
       {name}
     </td>
     <td>
-      <Sparklines height={120} width={180} data={temps}>
-        <SparklinesLine color="red" />
-      </Sparklines>
+      <Chart data={temps} color="orange" units="K" />
+    </td>
+    <td>
+      <Chart data={pressures} color="blue" units="hPa" />
+    </td>
+    <td>
+      <Chart data={humidities} color="black" units="%" />
     </td>
   </tr>
 )
@@ -28,10 +37,10 @@ return (
       <table className='table table-hover'>
         <thead>
           <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th width={200}>City</th>
+            <th  width={200}>Temperature (K)</th>
+            <th  width={200}>Pressure (hPa)</th>
+            <th  width={200}>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
