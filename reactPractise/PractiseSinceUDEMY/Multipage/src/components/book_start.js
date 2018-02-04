dc6a965a56d13f25686e65858c1e8fc416d3e3d1
Laura <playeronereadystart@gmail.com>
2018-02-04 16:52:59 +0000

    Attempts at better styling

--- reactPractise/PractiseSinceUDEMY/Multipage/src/components/book_start.js
+++ reactPractise/PractiseSinceUDEMY/Multipage/src/components/book_start.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+
  class BookStart extends Component {
   render() {
     return (
-      <div>
-        <div className="float-left">
-          <h1>BOOK OF THE WEEK</h1>
-          <img src={this.props.book.img}/>
-        </div>
-        <div className='float-left'>
-          <h2>{this.props.book.title}</h2>
-          <h4>{this.props.book.author}</h4>
-          <Link className="btn btn-primary" to='/aboutbook'>
+
+  <div className="container">
+    <div className="row">
+      <div className="col-xs-6 ">
+        <h1 >BOOK OF THE WEEK</h1>
+        <h2 >{this.props.book.title}</h2>
+        <h4>{this.props.book.author}</h4>
+        <Link className="btn btn-primary" to='/aboutbook'>
             What's it about...
-          </Link>
-        </div>
+        </Link>
       </div>
+      <div className="col-xs-6 ">
+        <img src={this.props.book.img}/>
+      </div>
+    </div>
+   </div>
+
     );
   }
 }
