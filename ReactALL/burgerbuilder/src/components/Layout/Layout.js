54c4a5edad53251609fb275f3cb93f5a22d80209
Laura <playeronereadystart@gmail.com>
2018-02-26 17:13:15 +0000

    Beginning the build a burger app

---
+++ ReactALL/burgerbuilder/src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+
+const Layout = (props) => (
+  <Aux>
+    <div>ToolBar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
+      {props.children}
+    </main>
+  </Aux>
+);
+
+export default Layout;
