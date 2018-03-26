6f2b75392ae73c12cd1c7a6120e3651eca8b1f36
Laura <playeronereadystart@gmail.com>
2018-03-26 18:53:29 +0100

    Starting on the framework.

---
+++ javascript/UdemPractice/J2_StructuringSafeCode/Starter/Greetr.js
@@ -0,0 +1,22 @@
+//constructed framework.
+
+(function(global, $){
+
+  var Greetr = function(firstName, lastName, language){
+    return new Greetr.init(firstName, lastName, language);
+  }
+
+  Greetr.prototype = {};
+
+
+  Greetr.init = function(firstName, lastName,language){
+    var self = this;
+    self.firstName = firstName ||  '';
+    self.lastName = lastName ||  '';
+    self.language = language ||  'en';
+
+  }
+
+  Greetr.init.prototype = Greetr.prototype;
+
+}(window, jQuery));
---
+++ javascript/UdemPractice/J2_StructuringSafeCode/Starter/app.js
@@ -0,0 +1,1 @@
+G$(firstname, lastname, language);
