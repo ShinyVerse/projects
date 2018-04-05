a7bbcb7e48c51993106df1a6d0090b781ae0023b
Laura <playeronereadystart@gmail.com>
2018-04-05 20:36:51 +0100

    Sorted out: must choose operator

--- gitpage/js/main.js
+++ gitpage/js/main.js
@@ -3,6 +3,7 @@ var navLine = document.getElementById("navLine");
 var hamburger = document.getElementById("menuButton");
 var calcScreen = document.getElementById("calcScreen");
 var calcCase = document.getElementById("calculatorCase");
+var symbols = document.getElementsByClassName("symbol");
 
 //called when hamburger is clicked on
 function toggleNav() {
@@ -22,14 +23,22 @@ calcCase.onclick = function() {
 };
 
 var fresh = true;
-  // calcScreen.innerHTML = calcSoFar action jointArr;
+// calcScreen.innerHTML = calcSoFar action jointArr;
 var secondNum = [];
 var firstNum = [];
 var buttonPressed = "";
 var first = 0;
 var second = 0;
-var store = {
+var store = {}
 
+function clearCalcAll() {
+  firstNum = [];
+  secondNum = [];
+  buttonPressed = "";
+  fresh = true;
+  first = 0;
+  second = 0;
+  calcScreen.innerHTML = 0;
 }
 
 //depending on button on calcCase clicked.
@@ -45,85 +54,95 @@ function check(result) {
     case "7":
     case "8":
     case "9":
-    if (fresh){
-      firstNum.push(result);
-      calcScreen.innerHTML = firstNum.join("");
-      first = parseInt(firstNum.join(""));
-    } else {
-      secondNum.push(result);
-      calcScreen.innerHTML = secondNum.join("");
-      second = parseInt(secondNum.join(""));
-      console.log(buttonPressed, first, second);
-    }
+      if (fresh) {
+        firstNum.push(result);
+        calcScreen.innerHTML = firstNum.join("");
+        first = parseInt(firstNum.join(""));
+      } else {
+        if (buttonPressed === "") {
+          setTimeout(function() {
+            calcScreen.innerHTML = first
+          }, 50);
+
+          Array.prototype.map.call(symbols, function(symbol) {
+            symbol.classList.add("symbolError");
+            setTimeout(function() {
+              symbol.classList.remove("symbolError")
+            }, 500);
+          });
+          calcScreen.innerHTML = secondNum.join("");
+          return calcScreen.innerHTML = secondNum.join("");
+
+        }
+        secondNum.push(result);
+        calcScreen.innerHTML = secondNum.join("");
+        second = parseInt(secondNum.join(""));
+        console.log(buttonPressed, first, second);
+      }
 
       break;
     case "÷":
-    fresh = false;
-    buttonPressed = "divide";
-    calcScreen.innerHTML = 0;
+      fresh = false;
+      buttonPressed = "divide";
+      calcScreen.innerHTML = 0;
       break;
     case "x":
-    fresh = false;
-    buttonPressed = "times";
-    calcScreen.innerHTML = 0;
+      fresh = false;
+      buttonPressed = "times";
+      calcScreen.innerHTML = 0;
       break;
     case "-":
-    fresh = false;
-    buttonPressed = "minus";
-    calcScreen.innerHTML = 0;
+      fresh = false;
+      buttonPressed = "minus";
+      calcScreen.innerHTML = 0;
       break;
     case "+":
-    fresh = false;
-    buttonPressed = "plus";
-    calcScreen.innerHTML = 0;
+      fresh = false;
+      buttonPressed = "plus";
+      calcScreen.innerHTML = 0;
       break;
     case "C":
-    firstNum = [];
-    secondNum = [];
-    buttonPressed = "";
-    fresh = true;
-    first = 0;
-    second = 0;
-    calcScreen.innerHTML = 0;
+      clearCalcAll();
 
       break;
     case "=":
-     checkOperator(buttonPressed, first, second);
+      checkOperator(buttonPressed, first, second);
+      buttonPressed = "";
       break;
     default:
-      alert("default");
+
   }
 }
 function checkOperator(operator, a, b) {
   switch (operator) {
     case "divide":
-    calcScreen.innerHTML = a / b;
-    first = calcScreen.innerHTML = a / b;
-    secondNum = [];
-    buttonPressed = "";
+      calcScreen.innerHTML = a / b;
+      first = calcScreen.innerHTML = a / b;
+      secondNum = [];
+      buttonPressed = "";
       //  function divide(a, b){
       // return calcScreen.innerHTML = a / b;
-    // }
+      // }
       break;
     case "times":
-    calcScreen.innerHTML = a * b;
-    first = calcScreen.innerHTML = a * b;
-    secondNum = [];
-    buttonPressed = "";
+      calcScreen.innerHTML = a * b;
+      first = calcScreen.innerHTML = a * b;
+      secondNum = [];
+      buttonPressed = "";
       break;
     case "minus":
-    calcScreen.innerHTML = a - b;
-    first = calcScreen.innerHTML = a - b;
-    secondNum = [];
-    buttonPressed = "";
+      calcScreen.innerHTML = a - b;
+      first = calcScreen.innerHTML = a - b;
+      secondNum = [];
+      buttonPressed = "";
       break;
     case "plus":
-    calcScreen.innerHTML = a + b;
-    first = calcScreen.innerHTML = a + b;
-    secondNum = [];
-    buttonPressed = "";
+      calcScreen.innerHTML = a + b;
+      first = calcScreen.innerHTML = a + b;
+      secondNum = [];
+      buttonPressed = "";
       break;
     default:
-      alert("default");
+      clearCalcAll();
   }
 }
