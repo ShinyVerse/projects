4bac9320417bb5b3830e21161503540b60a22d0a
Laura Jackson <playeronereadystart@gmail.com>
2018-05-09 09:37:40 +0100

    Algorithm: needs refactoring.

---
+++ algorithms/HTMLEntities/main.js
@@ -0,0 +1,36 @@
+
+function convertHTML(str) {
+  var returnedStr = str;
+  var regAnd = new RegExp(/[&]/g);
+  var regLArrow = new RegExp(/[<]/g);
+  var regRArrow = new RegExp(/[>]/g);
+  var regSingleQ = new RegExp(/[']/g);
+  var regDoubleQ = new RegExp(/["]/g);
+if (regAnd.test(returnedStr)){
+  let newStr = returnedStr.replace(/[&]/g, '&amp');
+  returnedStr = newStr;
+}
+if (regLArrow.test(returnedStr)){
+  let newStr = returnedStr.replace(/[<]/g, '&lt');
+  returnedStr = newStr;
+}
+if (regRArrow.test(returnedStr)){
+  let newStr = returnedStr.replace(/[>]/g, '&gt');
+  returnedStr = newStr;
+}
+if (regSingleQ.test(returnedStr)){
+  let newStr = returnedStr.replace(/[']/g, '&apos');
+  returnedStr = newStr;
+}
+if (regDoubleQ.test(returnedStr)){
+  let newStr = returnedStr.replace(/["]/g, '&quot');
+  returnedStr = newStr;
+}
+
+
+
+
+return returnedStr;
+}
+
+convertHTML("Dolce & & Gabbana");
