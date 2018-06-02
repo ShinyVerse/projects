f8d53869fddd95ed7a7e85badca8c16c2d469fa8
Laura Jackson <playeronereadystart@gmail.com>
2018-06-02 15:15:55 +0100

    work on calculator

--- calc/main3.js
+++ calc/main3.js
@@ -1,4 +1,3 @@
-
 var calcCase = document.getElementById("calculatorCase");
 var symbols = document.getElementsByClassName("symbol");
 var calcScreen = document.getElementById("calcScreen1");
@@ -31,161 +30,208 @@ function check(keyed) {
     case "7":
     case "8":
     case "9":
-    if (expectingSymb){
-      Array.prototype.map.call(symbols, function(symbol) {
-        symbol.classList.add("symbolError");
-        setTimeout(function() {
-          symbol.classList.remove("symbolError")
-        }, 500);
-      });
-      break;
-    }
-    numArr.push(keyed);
-    if(fresh){
-      calcScreen2.innerHTML = numArr.join("");
-      fresh = false;
-      expectingNum = false;
-    } else {
-      let tempNum = numArr.join("");
-      calcScreen2.innerHTML =  fullArr.join("") + tempNum;
-      expectingNum = false;
-    }
-      break;
-    case "÷":
-
-    if (expectingSymb){
-      expectingSymb = false;
-    }
-    if (!fresh){
-      if (expectingNum){
+      if (numArr[numArr.length - 1] === "0") {
         break;
       }
-      num = numArr.join("");
-      fullArr.push(num);
-      fullArr.push("/");
-      expectingNum = true;
-      numArr = [];
-      num = null;
-      calcScreen2.innerHTML = fullArr.join("");
+      if (expectingSymb) {
+        Array.prototype.map.call(symbols, function(symbol) {
+          symbol.classList.add("symbolError");
+          setTimeout(function() {
+            symbol.classList.remove("symbolError")
+          }, 500);
+        });
+        break;
+      }
+      numArr.push(keyed);
+      if (fresh) {
+        calcScreen2.innerHTML = numArr.join("");
+        fresh = false;
+        expectingNum = false;
+      } else {
+        let tempNum = numArr.join("");
+        calcScreen2.innerHTML = fullArr.join("") + tempNum;
+        expectingNum = false;
+      }
+      break;
+    case ".":
+
+    if (numArr[numArr.length-2] === "."){
+      break;
     }
+    let num2test = parseInt(numArr[numArr.length-1]);
+    let tester = new RegExp(/[0-9]/);;
+    let result = tester.test(num2test);
+      // let checkLastInput = parseInt(numArr[numArr.length-1]);
+      if( result){
+        numArr.push(".");
+        expectingNum = true;
+        if (fullArr.length < 1){
+          calcScreen2.innerHTML = numArr.join("");
+          break;
+        }
+        calcScreen2.innerHTML = fullArr.join("") + numArr.join("");
+      }
+      break;
+    case "÷":
+      if (expectingSymb) {
+        expectingSymb = false;
+      }
+      if (!fresh) {
+        if (expectingNum) {
+          fullArr.pop();
+          fullArr.push("/")
+          calcScreen2.innerHTML = fullArr.join("");
+          break;
+        }
+        num = numArr.join("");
+        fullArr.push(num);
+        fullArr.push("/");
+        expectingNum = true;
+        numArr = [];
+        num = null;
+        calcScreen2.innerHTML = fullArr.join("");
+      }
       break;
     case "x":
 
-    if (expectingSymb){
-      expectingSymb = false;
-    }
-    if (!fresh){
-      if (expectingNum){
-        break;
+      if (expectingSymb) {
+        expectingSymb = false;
       }
-      num = numArr.join("");
-      fullArr.push(num);
-      fullArr.push("*");
-      expectingNum = true;
-      numArr = [];
-      num = null;
-      calcScreen2.innerHTML = fullArr.join("");
-    }
+      if (!fresh) {
+        if (expectingNum) {
+          fullArr.pop();
+          fullArr.push("x");
+          calcScreen2.innerHTML = fullArr.join("");
+          break;
+        }
+        num = numArr.join("");
+        fullArr.push(num);
+        fullArr.push("*");
+        expectingNum = true;
+        numArr = [];
+        num = null;
+        calcScreen2.innerHTML = fullArr.join("");
+      }
       break;
     case "-":
-    if (expectingSymb){
-      expectingSymb = false;
-    }
-    if (!fresh){
-      if (expectingNum){
-        break;
+      if (expectingSymb) {
+        expectingSymb = false;
       }
-      num = numArr.join("");
-      fullArr.push(num);
-      fullArr.push("-");
-      expectingNum = true;
-      numArr = [];
-      num = null;
-      calcScreen2.innerHTML = fullArr.join("");
-    }
+      if (!fresh) {
+        if (expectingNum) {
+          fullArr.pop();
+          fullArr.push("-");
+          calcScreen2.innerHTML = fullArr.join("");
+          break;
+        }
+        num = numArr.join("");
+        fullArr.push(num);
+        fullArr.push("-");
+        expectingNum = true;
+        numArr = [];
+        num = null;
+        calcScreen2.innerHTML = fullArr.join("");
+      }
       break;
     case "+":
 
-    if (expectingSymb){
-      expectingSymb = false;
-    }
-    if (!fresh){
-      if (expectingNum){
-        break;
+      if (expectingSymb) {
+        expectingSymb = false;
       }
-      num = numArr.join("");
-      fullArr.push(num);
+      if (!fresh) {
+        if (expectingNum) {
+          fullArr.pop();
+          fullArr.push("+");
+          calcScreen2.innerHTML = fullArr.join("");
+          break;
+        }
+        num = numArr.join("");
+        fullArr.push(num);
 
-      fullArr.push("+");
-      expectingNum = true;
-      numArr = [];
-      num = null;
-      calcScreen2.innerHTML = fullArr.join("");
-    }
+        fullArr.push("+");
+        expectingNum = true;
+        numArr = [];
+        num = null;
+        calcScreen2.innerHTML = fullArr.join("");
+      }
       break;
     case "C":
-     fullArr = [];
-     numArr = [];
-     sym = "";
-     num = null;
-   mainResult = 0;
-    fresh = true;
-    mainRezFlag = false;
-    calcScreen2.innerHTML = " ";
-    calcScreen1.innerHTML = " ";
-    expectingSymb = false;
-    console.log(fullArr);
+      fullArr = [];
+      numArr = [];
+      sym = "";
+      num = null;
+      mainResult = 0;
+      fresh = true;
+      mainRezFlag = false;
+      calcScreen2.innerHTML = " ";
+      calcScreen1.innerHTML = " ";
+      expectingSymb = false;
+      console.log(fullArr);
       break;
     case "=":
-    num = numArr.join("");
-    fullArr.push(num);
-    console.log("fullArr before = " +fullArr);
-    numArr = [];
-    num = null;
-    if (!fresh && fullArr.length > 2){
-      calcAll(fullArr, 0, false);
+    if(expectingNum){
+      break;
     }
-    fullArr = [];
-    num = null;
-    numArr = [];
-    numArr.push(mainResult);
-    mainResult = 0;
-    expectingSymb = true;
-    calcScreen1.innerHTML = numArr.join("");
-    calcScreen2.innerHTML = numArr.join("");
+      num = numArr.join("");
+      fullArr.push(num);
+      console.log("fullArr before = " + fullArr);
+      numArr = [];
+      num = null;
+      if (!fresh && fullArr.length > 2) {
+        calcAll(fullArr, 0, false);
+      }
+      fullArr = [];
+      num = null;
+      numArr = [];
+      numArr.push(mainResult);
+      mainResult = 0;
+      expectingSymb = true;
+      calcScreen1.innerHTML = numArr.join("");
+      calcScreen2.innerHTML = numArr.join("");
       break;
     default:
   }
 
 }
 
-function calcAll(arr, extra, flag){
+function calcAll(arr, extra, flag) {
   var main = 0;
-  for (var i = 0; i < 2; i++){
-    if (extra > 0){
+  for (var i = 0; i < 2; i++) {
+    if (extra > 0 || extra < 0 || extra === 0 && flag === true) {
       let pressed = arr[i];
       let a = extra;
-      let b = parseFloat(arr[i+1]);
-      let result = operatorChecker(a,b, pressed);
+      let b = parseFloat(arr[i + 1]);
+      let result = operatorChecker(a, b, pressed);
       let newArr = arr.slice(2);
-      if (newArr.length > 1){
+      if (newArr.length > 1) {
         calcAll(newArr, result, true);
       } else {
-        console.log("rez" + result);
-        return mainResult = result;
+        if (result % 1 === 0){
+          calcScreen1.innerHTML = result;
+          return mainResult = result;
+        } else {
+          calcScreen1.innerHTML = result.toFixed(2);
+          return mainResult = result.toFixed(2);
+        }
       }
     }
-    if (extra === 0 && flag !== true){
-      let pressed = arr[i+1];
+    if (extra === 0 && flag !== true) {
+      let pressed = arr[i + 1];
       let a = parseFloat(arr[i]);
-      let b = parseFloat(arr[i+2]);
-      let result = operatorChecker(a,b, pressed);
+      let b = parseFloat(arr[i + 2]);
+      let result = operatorChecker(a, b, pressed);
       let newArr = arr.slice(3);
-      if (newArr.length > 1){
+      if (newArr.length > 1) {
         calcAll(newArr, result, true);
-      }  else {
-        calcScreen1.innerHTML = result;
-        return mainResult = result;
+      } else {
+        if (result % 1 === 0){
+          calcScreen1.innerHTML = result;
+          return mainResult = result;
+        } else {
+          calcScreen1.innerHTML = result.toFixed(2);
+          return mainResult = result.toFixed(2);
+        }
+
       }
     }
 
@@ -193,15 +239,15 @@ function calcAll(arr, extra, flag){
   return;
 }
 
-function operatorChecker(a, b, pressed){
-    switch (pressed) {
-      case "*":
+function operatorChecker(a, b, pressed) {
+  switch (pressed) {
+    case "*":
       return a * b;
-      case "+":
+    case "+":
       return a + b;
-      case "-":
+    case "-":
       return a - b;
-      case "/":
+    case "/":
       return a / b;
   }
 }
